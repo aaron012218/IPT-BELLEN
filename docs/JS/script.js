489ad0a9aaf57edc35b9638a4b9f1feee741448b
BSIT3B_BELLEN <aeronotso@gmail.com>
2024-11-28 17:54:39 +0800

    MY PORTFOLIO

---
+++ docs/JS/script.js
@@ -0,0 +1,13 @@
+// Get the elements from the HTML
+const nameElement = document.getElementById("name");
+const titleElement = document.getElementById("title");
+const descriptionElement = document.getElementById("description");
+const skillsElement = document.getElementById("skills");
+const hireButton = document.getElementById("hire-button");
+
+// Set the values of the elements
+nameElement.innerHTML = "Aaron";
+titleElement.innerHTML = "Web Developer";
+descriptionElement.innerHTML = "Currently diving into the world of coding and software development. I'm passionate about learning new technologies and building projects that challenge me to grow. As I continue to explore different programming languages and frameworks, I'm always looking for ways to improve and expand my skills. Excited to see where this journey takes me!";
+skillsElement.innerHTML = "HTML, CSS, JavaScript, React, Node.js";
+hireButton.innerHTML = "Hire me";
